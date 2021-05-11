19a816223c3848fd255d12244f70ff25695f4784
panther-swap <admin@pantherswap.com>
2021-05-11 10:30:11 +0800

    refactor: Change block subgraph

--- utils/blocks/client.ts
+++ utils/blocks/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.thegraph.com/subgraphs/name/pancakeswap/blocks",
+    uri: "https://api.bscgraph.org/subgraphs/name/bsc-blocks",
   }),
   cache: new InMemoryCache(),
 });
